80711b6e876702d0c099481e814303a539e02b5c
George Walter Colgrove IV <georgecolgrove@outlook.com>
2026-03-31 19:41:07 -0400

    fixes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.activiti.org',
+  url: 'https://captainglac1er.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: process.env.BASE_URL || '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
